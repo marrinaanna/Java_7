2d729b6849d002d6aeba4bd7cf94e351345005e8
Анна <marinaanna542@gmail.com>
2022-06-10 14:17:49 +0300

    BreakTest

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -11,7 +11,7 @@ public class StatisticsServiceTest {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long expected = 8;
 
         long actual = service.findMax(incomesInBillions);
 
